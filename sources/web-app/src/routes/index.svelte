a77877fc27cd99db3dc118ab8fd889cfce2d2753
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2021-01-03 03:23:07 +0100

    WIP: index.svelte
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

--- sources/web-app/src/routes/index.svelte
+++ sources/web-app/src/routes/index.svelte
@@ -1,10 +1,43 @@
 <script>
-  
+  import Sidebar from '../components/Sidebar.svelte';
+  import Items from '../components/Items.svelte';
 </script>
 
 <style>
+  article {
+    display: grid;
+    grid-template-columns: 1fr 11fr;
+    grid-template-rows: 1fr;
+    grid-gap: min(1vw, 1vh);
+    grid-template-areas:
+      'sidebar contents'
+    ;
+
+    height: 100%;
+  }
+
+  aside {
+    grid-area: sidebar;
+
+    background-color:aquamarine;
+  }
+
+  section {
+    grid-area: contents;
+
+    background-color:burlywood;
+  }
 </style>
 
 <svelte:head>
   <title>Macarons Shop Berlin</title>
 </svelte:head>
+
+<article>
+  <aside>
+    <Sidebar />
+  </aside>
+  <section>
+    <Items />
+  </section>
+</article>
